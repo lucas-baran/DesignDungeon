90b71a354a38b1884eb9e88230a511c66cdb1fc0
Lucas Baran <lucas.baran7@gmail.com>
2022-11-12 22:15:46 +0100

    add name and description to weapons

--- Assets/Scripts/Scriptables/WeaponData.cs
+++ Assets/Scripts/Scriptables/WeaponData.cs
@@ -5,11 +5,15 @@ public sealed class WeaponData : ScriptableObject
 {
     // -- FIELDS
 
+    [SerializeField] private string _name = "Skill";
+    [SerializeField, Multiline] private string _description = null;
     [SerializeField] private Sprite _sprite = null;
     [SerializeField] private AbilityData _effectiveSkill = null;
 
     // -- PROPERTIES
 
+    public string Name => _name;
+    public string Description => _description;
     public Sprite Sprite => _sprite;
     public AbilityData EffectiveSkill => _effectiveSkill;
 }
