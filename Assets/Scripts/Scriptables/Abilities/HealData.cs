fe502e54b9f1239d70090f17652f3ffc3dd500fc
Lucas Baran <lucas.baran7@gmail.com>
2022-12-01 21:52:03 +0100

    add health potion ability

--- Assets/Scripts/Scriptables/Abilities/HealData.cs
+++ Assets/Scripts/Scriptables/Abilities/HealData.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 public sealed class HealData : AbilityData
 {
     // -- FIELDS
-
+    
     [SerializeField] private int _heal = 1;
+
+    // -- PROPERTIES
+
+    public int Heal => _heal;
 }
