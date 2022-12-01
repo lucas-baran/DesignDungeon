d802104dd7834118d2c3c36f6a869d764b58f49a
GGestin <xngt974@gmail.com>
2022-12-01 22:21:29 +0100

    Build Settings

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -70,7 +70,6 @@ public sealed class PlayerInput : MonoBehaviour
         if( Input.GetKeyDown( InputSystem.Instance.GetKey(EKeyActionType.SkipDialogue ) ) )
         {
             OnNextDialogueDown?.Invoke();
-            AudioManager.Instance.PlaySound( ESoundType.AzzuroAngry );
         }
 
         if( InputLocked )
@@ -104,7 +103,6 @@ public sealed class PlayerInput : MonoBehaviour
         if( Input.GetKeyDown( InputSystem.Instance.GetKey( EKeyActionType.WeaponAbility ) ) )
         {
             OnAbilityDown?.Invoke( EAbilityCategory.Weapon );
-            AudioManager.Instance.PlaySound( ESoundType.Attack );
         }
 
         if( Input.GetKeyDown( InputSystem.Instance.GetKey( EKeyActionType.MovementAbility ) ) )
